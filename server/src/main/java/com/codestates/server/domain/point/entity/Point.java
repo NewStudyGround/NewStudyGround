49df0d7c84455573e62294dc6860cf4d810e90c1
hae02y <godud1118@gmail.com>
2023-11-05 19:44:12 +0900

    feat: point entity생성

---
+++ server/src/main/java/com/codestates/server/domain/point/entity/Point.java
@@ -0,0 +1,16 @@
+package com.codestates.server.domain.point.entity;
+
+import com.codestates.server.domain.member.entity.Member;
+
+import javax.persistence.*;
+
+@Entity
+public class Point {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Member member;
+}
