e539e7d49e0795fbd2121cf7c549927f8f9f098b
luminousol <luminousolding@gmail.com>
2023-11-21 09:08:46 +0900

    chore: MemberMapper adminDTO 추가

--- server/src/main/java/com/codestates/server/domain/member/mapper/MemberMapper.java
+++ server/src/main/java/com/codestates/server/domain/member/mapper/MemberMapper.java
@@ -1,11 +1,7 @@
 package com.codestates.server.domain.member.mapper;
 
 
-import com.codestates.server.domain.member.dto.MemberImagePatchDto;
-import com.codestates.server.domain.member.dto.MemberBoardResponseDto;
-import com.codestates.server.domain.member.dto.MemberPatchDto;
-import com.codestates.server.domain.member.dto.MemberPostDto;
-import com.codestates.server.domain.member.dto.MemberResponseDto;
+import com.codestates.server.domain.member.dto.*;
 import com.codestates.server.domain.member.entity.Member;
 import org.mapstruct.Mapper;
 
@@ -25,4 +21,8 @@ public interface MemberMapper {
     List<MemberResponseDto> membersTomemberResponseDto(List<Member> members);
 
     MemberBoardResponseDto memberToMemberBoardResponseDto(Member member);
+
+    MemberAdminResponseDto memberToMemberAdminResponseDto(Member member);
+
+    List<MemberAdminListResponseDto> membersToMemberAdminListResponseDto(List<Member> members);
 }
